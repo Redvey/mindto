78d5f1e9a6e846058fd0cc789731cb11d0dd86ea
redvey <roopam12cse@gmail.com>
2024-05-04 11:12:14 +0530

    Initialized files in Bloc architecture

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:mindto/design/app_theme.dart';
+import 'package:mindto/features/onboarding/ui/onboarding_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: AppTheme.darkTheme,
+      home: OnboardingScreen(),
+    );
+  }
+}
